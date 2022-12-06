a18f0f610ec688ba30b2f34093cda72a9d465390
gerald.schuster <gerald.schuster@pm.me>
2022-12-06 06:10:39 +0100

    Day06

---
+++ Day06/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = File.ReadAllText("input.txt");
+
+Console.Out.WriteLine(Find(input, 4));
+Console.Out.WriteLine(Find(input, 14));
+
+int Find(string signal, int length) {
+  for (var i = 0; i < signal.Length - length; i++) {
+    var sub = signal.Substring(i, length);
+    if (sub.ToArray().Distinct().Count() == length) {
+      return i + length;
+    }
+  }
+  return -1;
+}
