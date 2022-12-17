39dcf020f2bb381474b0932f38adfca4fd79be61
gerald.schuster <gerald.schuster@pm.me>
2022-12-17 10:35:01 +0100

    Day17

---
+++ Day16/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+const string regex = @"Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.+)";
+
+var tunnels = new List<Tunnel>();
+
+foreach (var line in input) {
+  var match = Regex.Match(line, regex);
+  tunnels.Add(new Tunnel {
+    Name = match.Groups[1].Value, 
+    FlowRate = int.Parse(match.Groups[2].Value), 
+    Tunnels = match.Groups[3].Value
+      .Split(",")
+      .Select(t => t.Trim())
+      .ToList()
+  });
+}
+
+var shortestPaths = new Dictionary<Tunnel, Dictionary<Tunnel, int>>();
+
+foreach (var from in tunnels) {
+  foreach (var to in tunnels.Where(t => t != from)) {
+    var dist = int.MaxValue;
+    if (shortestPaths.ContainsKey(from) && shortestPaths[from].ContainsKey(to)) continue;
+    FindShortestPath(from, to, tunnels, new List<Tunnel> {from}, ref dist);
+    if (shortestPaths.ContainsKey(from)) {
+      shortestPaths[from].Add(to, dist);
+    } else {
+      shortestPaths[from] = new Dictionary<Tunnel, int> { { to, dist } };
+    }
+    if (shortestPaths.ContainsKey(to)) {
+      shortestPaths[to].Add(from, dist);
+    } else {
+      shortestPaths[to] = new Dictionary<Tunnel, int> { { from, dist } };
+    }
+  }
+}
+
+void FindShortestPath(Tunnel from, Tunnel to, List<Tunnel> map, List<Tunnel> path, ref int dist) {
+  if (from.Tunnels.Contains(to.Name)) {
+    if (path.Count < dist) {
+      dist = path.Count;
+    }
+  }
+  foreach (var tunnel in from.Tunnels.Where(t => !path.Contains(map.Single(x => x.Name == t)))) {
+    FindShortestPath(map.Single(x => x.Name == tunnel), to, map, path.Concat(new []{from}).ToList(), ref dist);
+  }
+}
+
+var start = tunnels.Single(x => x.Name == "AA");
+var pt1 = 0;
+OpenValves(start, 29, 0, ImmutableHashSet<Tunnel>.Empty.Union(tunnels.Where(x => x.FlowRate == 0)), ref pt1);
+Console.Out.WriteLine(pt1);
+
+var pt2 = 0;
+
+OpenValvesWithElephant(start, 25, 0, ImmutableHashSet<Tunnel>.Empty.Union(tunnels.Where(x => x.FlowRate == 0)), ref pt2, false);
+Console.Out.WriteLine(pt2);
+
+void OpenValves(Tunnel current, int minutesLeft, int currentFlow, ImmutableHashSet<Tunnel> open, ref int maxFlowRate) {
+  if (currentFlow > maxFlowRate) {
+    maxFlowRate = currentFlow;
+  }
+  if (minutesLeft <= 0) return;
+
+  if (!open.Contains(current)) {
+    OpenValves(current, minutesLeft - 1, currentFlow + current.FlowRate * minutesLeft, open.Union(new []{current}), ref maxFlowRate);
+  } else {
+    foreach (var (key, value) in shortestPaths[current].Where(x => !open.Contains(x.Key))) {
+      OpenValves(key, minutesLeft - value, currentFlow, open, ref maxFlowRate);
+    }
+  }
+}
+
+void OpenValvesWithElephant(Tunnel current, int minutesLeft, int currentFlow, ImmutableHashSet<Tunnel> open, ref int maxFlowRate, bool elephant) {
+  if (currentFlow > maxFlowRate) {
+    maxFlowRate = currentFlow;
+    Console.Out.WriteLine(maxFlowRate);
+  }
+  if (minutesLeft <= 0) return;
+
+  if (!open.Contains(current)) {
+    OpenValvesWithElephant(current, minutesLeft - 1, currentFlow + current.FlowRate * minutesLeft, open.Union(new []{current}), ref maxFlowRate, elephant);
+    if (!elephant) {
+      OpenValvesWithElephant(tunnels.Single(x => x.Name == "AA"), 25, currentFlow + current.FlowRate * minutesLeft, open.Union(new []{current}), ref maxFlowRate, true);
+    }
+  } else {
+    foreach (var (key, value) in shortestPaths[current].Where(x => !open.Contains(x.Key))) {
+      OpenValvesWithElephant(key, minutesLeft - value, currentFlow, open, ref maxFlowRate, elephant);
+    }
+  }
+}
+
+record Tunnel {
+  public string Name { get; init; }
+  public int FlowRate { get; init; }
+  public IList<string> Tunnels { get; set; }
+}
---
+++ Day17/Program.cs
@@ -0,0 +1,163 @@
+﻿var input = File.ReadAllText("input.txt").ToCharArray();
+var shapes = new List<Shape> {
+  new() {
+    Rocks = new List<Position> {
+      new() { X = 0, Y = 0 },
+      new() { X = 1, Y = 0 },
+      new() { X = 2, Y = 0 },
+      new() { X = 3, Y = 0 }
+    }
+  }, new() {
+    Rocks = new List<Position> {
+      new() { X = 1, Y = 0 },
+      new() { X = 0, Y = 1 },
+      new() { X = 1, Y = 1 },
+      new() { X = 2, Y = 1 },
+      new() { X = 1, Y = 2 }
+    }
+  }, new() {
+    Rocks = new List<Position> {
+      new() { X = 0, Y = 0 },
+      new() { X = 1, Y = 0 },
+      new() { X = 2, Y = 0 },
+      new() { X = 2, Y = 1 },
+      new() { X = 2, Y = 2 }
+    }
+  }, new() {
+    Rocks = new List<Position> {
+      new() { X = 0, Y = 0 },
+      new() { X = 0, Y = 1 },
+      new() { X = 0, Y = 2 },
+      new() { X = 0, Y = 3 }
+    }
+  }, new() {
+    Rocks = new List<Position> {
+      new() { X = 0, Y = 0 },
+      new() { X = 1, Y = 0 },
+      new() { X = 0, Y = 1 },
+      new() { X = 1, Y = 1 }
+    }
+  }
+};
+
+var jetIndex = 0;
+var blockCounter = 0L;
+var blockIndex = 0;
+var currentMaxHeight = -1L;
+var map = new HashSet<Position>();
+var cache = new Dictionary<Cache, (long, long)>();
+const long limit = 1000000000000L;
+var loopFound = false;
+
+while (blockCounter < limit) {
+  
+  if (!loopFound) {
+    var newCacheItem = new Cache {
+      ShapeIndex = blockIndex,
+      GustIndex = jetIndex,
+      Board0 = currentMaxHeight - map.Where(m => m.X == 0).MaxBy(m => m.Y)?.Y,
+      Board1 = currentMaxHeight - map.Where(m => m.X == 1).MaxBy(m => m.Y)?.Y,
+      Board2 = currentMaxHeight - map.Where(m => m.X == 2).MaxBy(m => m.Y)?.Y,
+      Board3 = currentMaxHeight - map.Where(m => m.X == 3).MaxBy(m => m.Y)?.Y,
+      Board4 = currentMaxHeight - map.Where(m => m.X == 4).MaxBy(m => m.Y)?.Y,
+      Board5 = currentMaxHeight - map.Where(m => m.X == 5).MaxBy(m => m.Y)?.Y,
+      Board6 = currentMaxHeight - map.Where(m => m.X == 6).MaxBy(m => m.Y)?.Y
+    };
+
+    if (cache.ContainsKey(newCacheItem)) {
+      loopFound = true;
+      var (loopStart, oldMaxHeight) = cache[newCacheItem];
+      
+      var diff = blockCounter - loopStart;
+      var maxHeightDiff = currentMaxHeight - oldMaxHeight;
+      var times = (limit - blockCounter) / diff;
+      currentMaxHeight += times * maxHeightDiff;
+      blockCounter += times * diff;
+      map.Add(new Position { X = 0, Y = currentMaxHeight - newCacheItem.Board0!.Value });
+      map.Add(new Position { X = 1, Y = currentMaxHeight - newCacheItem.Board1!.Value });
+      map.Add(new Position { X = 2, Y = currentMaxHeight - newCacheItem.Board2!.Value });
+      map.Add(new Position { X = 3, Y = currentMaxHeight - newCacheItem.Board3!.Value });
+      map.Add(new Position { X = 4, Y = currentMaxHeight - newCacheItem.Board4!.Value });
+      map.Add(new Position { X = 5, Y = currentMaxHeight - newCacheItem.Board5!.Value });
+      map.Add(new Position { X = 6, Y = currentMaxHeight - newCacheItem.Board6!.Value });
+    }
+    else {
+      cache.Add(newCacheItem, (blockCounter, currentMaxHeight));
+    }
+  }
+  
+  var block = shapes[blockIndex];
+  var position = new Position { X = 2, Y = currentMaxHeight + 4 };
+  var settled = false;
+
+  if (blockCounter == 2022) {
+    Console.Out.WriteLine(currentMaxHeight + 1);
+  }
+  
+  while (!settled) {
+    var jet = input[jetIndex];
+    jetIndex = (jetIndex + 1) % input.Length;
+    Position newPosition;
+    IList<Position> newRocks;
+    switch (jet) {
+      case '>':
+        newPosition = position with { X = position.X + 1 };
+        newRocks = GetRocksPositions(block, newPosition).ToList();
+        if (newRocks.Max(x => x.X) > 6 || newRocks.Intersect(map).Any()) {
+          newPosition = position;
+        }
+
+        position = newPosition;
+        break;
+      case '<':
+        newPosition = position with { X = position.X - 1 };
+        newRocks = GetRocksPositions(block, newPosition).ToList();
+        if (newRocks.Min(x => x.X) < 0 || newRocks.Intersect(map).Any()) {
+          newPosition = position;
+        }
+
+        position = newPosition;
+        break;
+    }
+    newPosition = position with { Y = position.Y - 1 };
+    if (GetRocksPositions(block, newPosition).Intersect(map).Any() || newPosition.Y < 0) {
+      settled = true;
+      foreach (var p in GetRocksPositions(block, position)) {
+        map.Add(p);
+      }
+      blockCounter++;
+      blockIndex = (blockIndex+1)%5;
+      currentMaxHeight = map.Max(x => x.Y);
+    }
+    else {
+      position = newPosition;
+    }
+  }
+}
+
+Console.Out.WriteLine(currentMaxHeight + 1);
+
+IEnumerable<Position> GetRocksPositions(Shape shape, Position pos) {
+  return shape.Rocks.Select(r => new Position { X = pos.X + r.X, Y = pos.Y + r.Y });
+}
+
+internal record Shape {
+  public List<Position> Rocks { get; set; } = null!;
+}
+
+internal record Position {
+  public int X { get; set; }
+  public long Y { get; set; }
+}
+
+internal record Cache {
+  public int ShapeIndex { get; init; }
+  public int GustIndex { get; init; }
+  public long? Board0 { get; init; }
+  public long? Board1 { get; init; }
+  public long? Board2 { get; init; }
+  public long? Board3 { get; init; }
+  public long? Board4 { get; init; }
+  public long? Board5 { get; init; }
+  public long? Board6 { get; init; }
+}
