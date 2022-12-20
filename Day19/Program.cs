3109a697d6f6929718044e401e76cab2a8d0ba36
gerald.schuster <gerald.schuster@pm.me>
2022-12-20 07:32:13 +0100

    Day20

---
+++ Day19/Program.cs
@@ -0,0 +1,199 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+const string regex =
+    @"Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.";
+
+var blueprints = input.Select(line => {
+    var match = Regex.Match(line, regex);
+    return new Blueprint {
+        Index = int.Parse(match.Groups[1].Value),
+        OreRobot = new Costs {Ore = int.Parse(match.Groups[2].Value)},
+        ClayRobot = new Costs {Ore = int.Parse(match.Groups[3].Value)},
+        ObsidianRobot = new Costs {Ore = int.Parse(match.Groups[4].Value), Clay = int.Parse(match.Groups[5].Value)},
+        GeodeRobot = new Costs {Ore = int.Parse(match.Groups[6].Value), Obsidian = int.Parse(match.Groups[7].Value)}
+    };
+}).ToList();
+
+var pt1 = 0;
+
+Parallel.ForEach(blueprints, bp => {
+    var result = 0;
+    var inventory = new Inventory { OreRobot = 1 };
+    Mine(inventory, 24, null, bp, ref result);
+    pt1 += result * bp.Index;
+});
+
+Console.Out.WriteLine(pt1);
+
+var pt2 = 1;
+
+Parallel.ForEach(blueprints.Take(3), bp => {
+    var result = 0;
+    var inventory = new Inventory { OreRobot = 1 };
+    Mine(inventory, 32, null, bp, ref result);
+    pt2 *= result;
+});
+
+Console.Out.WriteLine(pt2);
+
+
+void Mine(Inventory inventory, int minute, Robot? toBuild, Blueprint blueprint, ref int result) {
+    while (true) {
+        if (minute <= 0) {
+            if (inventory.Geode > result) {
+                result = inventory.Geode;
+            }
+        }
+        else {
+            switch (toBuild) {
+                case Robot.Geode:
+                    if (CanBuildGeodeRobot(blueprint, inventory)) {
+                        inventory = inventory with { Ore = inventory.Ore - blueprint.GeodeRobot.Ore, Obsidian = inventory.Obsidian - blueprint.GeodeRobot.Obsidian };
+                        inventory = Collect(inventory);
+                        inventory = inventory with { GeodeRobot = inventory.GeodeRobot + 1 };
+                        foreach (var r in CanBuild(inventory)) {
+                            Mine(inventory, minute - 1, r, blueprint, ref result);
+                        }
+                    }
+                    else {
+                        inventory = Collect(inventory);
+                        minute = minute - 1;
+                        continue;
+                    }
+
+                    break;
+                case Robot.Ore:
+                    if (CanBuildOreRobot(blueprint, inventory)) {
+                        inventory = inventory with { Ore = inventory.Ore - blueprint.OreRobot.Ore };
+                        inventory = Collect(inventory);
+                        inventory = inventory with { OreRobot = inventory.OreRobot + 1 };
+                        foreach (var r in CanBuild(inventory)) {
+                            Mine(inventory, minute - 1, r, blueprint, ref result);
+                        }
+                    }
+                    else {
+                        inventory = Collect(inventory);
+                        minute = minute - 1;
+                        continue;
+                    }
+
+                    break;
+                case Robot.Clay:
+                    if (CanBuildClayRobot(blueprint, inventory)) {
+                        inventory = inventory with { Ore = inventory.Ore - blueprint.ClayRobot.Ore };
+                        inventory = Collect(inventory);
+                        inventory = inventory with { ClayRobot = inventory.ClayRobot + 1 };
+                        foreach (var r in CanBuild(inventory)) {
+                            Mine(inventory, minute - 1, r, blueprint, ref result);
+                        }
+                    }
+                    else {
+                        inventory = Collect(inventory);
+                        minute = minute - 1;
+                        continue;
+                    }
+
+                    break;
+                case Robot.Obsidian:
+                    if (CanBuildObsidianRobot(blueprint, inventory)) {
+                        inventory = inventory with { Ore = inventory.Ore - blueprint.ObsidianRobot.Ore, Clay = inventory.Clay - blueprint.ObsidianRobot.Clay };
+                        inventory = Collect(inventory);
+                        inventory = inventory with { ObsidianRobot = inventory.ObsidianRobot + 1 };
+                        foreach (var r in CanBuild(inventory)) {
+                            Mine(inventory, minute - 1, r, blueprint, ref result);
+                        }
+                    }
+                    else {
+                        inventory = Collect(inventory);
+                        minute = minute - 1;
+                        continue;
+                    }
+
+                    break;
+                case null:
+                    inventory = Collect(inventory);
+                    foreach (var r in CanBuild(inventory)) {
+                        Mine(inventory, minute - 1, r, blueprint, ref result);
+                    }
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(toBuild), toBuild, null);
+            }
+        }
+
+        break;
+    }
+}
+
+Inventory Collect(Inventory inventory) {
+    return inventory with {
+        Ore = inventory.Ore + inventory.OreRobot,
+        Clay = inventory.Clay + inventory.ClayRobot,
+        Obsidian = inventory.Obsidian + inventory.ObsidianRobot,
+        Geode = inventory.Geode + inventory.GeodeRobot
+    };
+}
+
+IEnumerable<Robot> CanBuild(Inventory inventory) {
+    if (inventory.ObsidianRobot > 0) yield return Robot.Geode;
+    if (inventory.ClayRobot > 0) yield return Robot.Obsidian;
+    if (inventory.OreRobot > 0) yield return Robot.Ore;
+    if (inventory.OreRobot > 0) yield return Robot.Clay;
+}
+
+bool CanBuildOreRobot(Blueprint bp, Inventory i) {
+    return i.Ore >= bp.OreRobot.Ore;
+}
+
+bool CanBuildClayRobot(Blueprint bp, Inventory i) {
+    return i.Ore >= bp.ClayRobot.Ore;
+}
+
+bool CanBuildObsidianRobot(Blueprint bp, Inventory i) {
+    return i.Ore >= bp.ObsidianRobot.Ore && i.Clay >= bp.ObsidianRobot.Clay;
+}
+
+bool CanBuildGeodeRobot(Blueprint bp, Inventory i) {
+    return i.Ore >= bp.GeodeRobot.Ore && i.Obsidian >= bp.GeodeRobot.Obsidian;
+}
+
+Console.Out.WriteLine("");
+
+
+
+internal record Inventory
+{
+    public int OreRobot { get; init; }
+    public int ClayRobot { get; init; }
+    public int ObsidianRobot { get; init; }
+    public int GeodeRobot { get; init; }
+    public int Ore { get; init; }
+    public int Clay { get; init; }
+    public int Obsidian { get; init; }
+    public int Geode { get; init; }
+}
+
+internal enum Robot {
+    Ore,
+    Clay,
+    Obsidian,
+    Geode
+}
+
+internal record Blueprint {
+    public int Index { get; init; }
+    public Costs OreRobot { get; init; }
+    public Costs ClayRobot { get; init; }
+    public Costs ObsidianRobot { get; init; }
+    public Costs GeodeRobot { get; init; }
+}
+
+internal record Costs
+{
+    public int Ore { get; init; }
+    public int Clay { get; init; }
+    public int Obsidian { get; init; }
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,76 @@
+﻿var list = new LinkedList<ListItem>(
+  File.ReadAllLines("input.txt")
+    .Select((s, index) => new ListItem {
+      Value = long.Parse(s), 
+      OriginalIndex = index
+    })
+  .ToList());
+
+var mixedList = Mix(list);
+
+var zero = mixedList.Single(l => l.Value == 0);
+var zeroIndex = IndexOf(mixedList, zero);
+var pt1 = mixedList.ElementAt((zeroIndex + 1000) % mixedList.Count).Value +
+          mixedList.ElementAt((zeroIndex + 2000) % mixedList.Count).Value +
+          mixedList.ElementAt((zeroIndex + 3000) % mixedList.Count).Value;
+
+Console.Out.WriteLine(pt1);
+
+const long key = 811589153;
+
+mixedList = new LinkedList<ListItem>(
+  File.ReadAllLines("input.txt")
+    .Select((s, index) => new ListItem {
+      Value = long.Parse(s) * key, 
+      OriginalIndex = index
+    })
+  .ToList());
+
+for (var c = 0; c < 10; c++) {
+  mixedList = Mix(mixedList);
+}
+
+zero = mixedList.Single(l => l.Value == 0);
+zeroIndex = IndexOf(mixedList, zero);
+var pt2 = mixedList.ElementAt((zeroIndex + 1000) % mixedList.Count).Value +
+          mixedList.ElementAt((zeroIndex + 2000) % mixedList.Count).Value +
+          mixedList.ElementAt((zeroIndex + 3000) % mixedList.Count).Value;
+
+Console.Out.WriteLine(pt2);
+
+LinkedList<ListItem> Mix(LinkedList<ListItem> items) {
+  for (var i = 0; i < items.Count; i++) {
+    var i1 = i;
+    var value = items.Single(k => k.OriginalIndex == i1);
+    var node = items.Find(value)!;
+    for (var m = 0; m < Math.Abs(value.Value % (items.Count - 1)); m++) {
+      if (value.Value > 0) {
+        var newNode = node.Next ?? node.List?.First!;
+        items.Remove(node);
+        items.AddAfter(newNode, node);
+      }
+      else {
+        var newNode = node.Previous ?? node.List?.Last!;
+        items.Remove(node);
+        items.AddBefore(newNode, node);
+      }
+    }
+  }
+  return items;
+}
+
+int IndexOf(IReadOnlyCollection<ListItem> linkedList, ListItem item) {
+  for (var i = 0; i < linkedList.Count; i++) {
+    if (linkedList.ElementAt(i) == item) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+internal record ListItem {
+  public long Value { get; init; }
+  public int OriginalIndex { get; init; }
+}
+
+
